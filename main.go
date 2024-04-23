3d8e4316fbd723e1d682e83783205fd1f0cd120d
Naresh <chakalinaresh1729@gmail.com>
2024-04-23 17:27:46 +0530

    add state to server

--- main.go
+++ main.go
@@ -5,12 +5,39 @@ import (
 	"net/http"
 )
 
+type apiConfig struct {
+	fileServerHits int
+}
+
+func (cfg *apiConfig) middlewareMetricInc(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits++
+		h.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	w.Header().Set("Content-Type", "text/palin; charset=utf-8")
+	w.Write([]byte("Hits: " + fmt.Sprint(cfg.fileServerHits)))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	w.Header().Set("Content-Type", "text/palin; charset=utf-8")
+	cfg.fileServerHits = 0
+	w.Write([]byte("Hits: " + fmt.Sprint(cfg.fileServerHits)))
+}
+
 func main() {
+	apiCfg := apiConfig{}
 	mux := http.NewServeMux()
 	corsMux := middlewareCors(mux)
 
-	mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/*", apiCfg.middlewareMetricInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("/healthz", healthHandler)
+	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("/reset", apiCfg.resetHandler)
 
 	fmt.Println("Starting server on 8080")
 	http.ListenAndServe(":8080", corsMux)
